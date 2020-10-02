660e31ec839637eb1a353908542184b6219b2ec4
AlecioP <alecioportaro@gmail.com>
2020-10-02 17:59:07 +0200

    Util script to present results of solver

---
+++ util/renderSchedule.py
@@ -0,0 +1,47 @@
+import csv
+
+D = []
+P = []
+L = []
+
+with open('minizinc.csv', newline='\n') as csvfile:
+    reader = csv.reader(csvfile, delimiter=',', quoting=csv.QUOTE_NONNUMERIC)
+    rn = 0
+    for r in reader:
+        for i in range(0,len(r)):
+            if(rn==0):#Reading Lengths array
+                L.append(r[i])
+            if(rn==1):#Reading days array
+                D.append(r[i])
+            if(rn==2):#Reading day positions array
+                P.append(r[i])
+        rn+=1
+
+for day in range(1,int(max(D)+1)):
+    F = []
+    ID = []
+    for i in range(0,len(P)):
+        if(D[i]==day):
+            F.append(P[i])
+            ID.append(i)
+    #Bubble sort
+    for i in range(0,len(F)):
+        for j in range(0,len(F)):
+            if(F[i]<F[j] and i>j ):
+                tmp = F[i]
+                F[i]=F[j]
+                F[j]=tmp
+                tmp = ID[i]
+                ID[i]=ID[j]
+                ID[j]=tmp
+    
+    partial = 0
+    print("Day "+str(day)+" : ")
+    for i in range(0,len(F)):
+        if(partial<240 and partial + L[ID[i]] >=240):
+            partial=240+L[ID[i]]
+            print("Event "+str(i)+" at 14:00 . Length : "+str(L[ID[i]]))
+        else:
+            print("Event "+str(i)+" at "+str(int(partial)//60+9)+":"+str(int(partial)%60)+" . Length : "+str(L[ID[i]]))
+            partial += L[ID[i]]
+
