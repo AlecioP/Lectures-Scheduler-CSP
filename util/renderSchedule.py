64b4c194ca1cc997886de6833f7ddaa8eb11ee0d
AlecioP <alecioportaro@gmail.com>
2022-06-07 19:17:17 +0200

    revised py script - model cleanup - trying some annotations

--- util/renderSchedule.py
+++ util/renderSchedule.py
@@ -1,10 +1,31 @@
 import csv
+import argparse
+
+parser = argparse.ArgumentParser(
+prog="Visualizer", 
+description="Create human readable visualization of scheduling contained in input file.",
+epilog="Author : https://github.com/AlecioP")
+
+parser.add_argument("--schedule","-s",
+dest="schedule_file",
+help="file containing the scheduling (default : ./minizinc.csv)",
+metavar="File",
+default="minizinc.csv")
+
+
+args = parser.parse_args()
+
+# Since Minizinc output has brackets in it
+# Just use SED command to remove those
+import os
+CMD = f"sed -Ee 's/[][]//g' -i {args.schedule_file}"
+os.system(CMD)
 
 D = []
 P = []
 L = []
 
-with open('minizinc.csv', newline='\n') as csvfile:
+with open(args.schedule_file, newline='\n') as csvfile:
     reader = csv.reader(csvfile, delimiter=',', quoting=csv.QUOTE_NONNUMERIC)
     rn = 0
     for r in reader:
@@ -36,12 +57,12 @@ for day in range(1,int(max(D)+1)):
                 ID[j]=tmp
     
     partial = 0
-    print("Day "+str(day)+" : ")
+    print(f"Day {day} : ")
     for i in range(0,len(F)):
         if(partial<240 and partial + L[ID[i]] >=240):
             partial=300+L[ID[i]]
-            print("Event "+str(i)+" at 14:00 . Length : "+str(int(L[ID[i]])//60)+"h "+str(int(L[ID[i]])%60)+"m")
+            print(f" E{ID[i]+1} 14:00 {int(L[ID[i]])//60}h{int(L[ID[i]])%60}m -> |",end="")
         else:
-            print("Event "+str(i)+" at "+str(int(partial)//60+9)+":"+str(int(partial)%60)+" . Length : "+str(int(L[ID[i]])//60)+"h "+str(int(L[ID[i]])%60)+"m")
+            print(f" E{ID[i]+1} {int(partial)//60+9}:{int(partial)%60} {int(L[ID[i]])//60}h{int(L[ID[i]])%60}m -> |",end="")
             partial += L[ID[i]]
-
+    print("")
